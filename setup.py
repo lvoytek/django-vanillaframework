063b4685fb8ca6993704f69389ef9dd730e2b231
Lena Voytek <lena@voytek.dev>
2022-11-19 16:49:02 -0700

    Add base setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='django-vanillaframework',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/lvoytek/django-vanillaframework',
+    license='LGPL v3',
+    author='Lena Voytek',
+    author_email='lena@voytek.dev',
+    description='Vanilla Framework frontend for Django',
+    install_requires=[
+        'django'
+    ]
+)
