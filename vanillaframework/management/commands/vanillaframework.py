bc68f24c1a92f2db9278fc20b7a3dddc4db421f9
Lena Voytek <lena@voytek.dev>
2022-11-25 14:35:04 -0700

    Add sass npm downloader

--- vanillaframework/management/commands/vanillaframework.py
+++ vanillaframework/management/commands/vanillaframework.py
@@ -1,6 +1,6 @@
 """Vanilla Framework management command."""
 import os.path
-
+import subprocess
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from pathlib import Path
@@ -30,7 +30,7 @@ class Command(BaseCommand):
 
     def handle_install(self, *args, **options):
         """Handle Vanilla Framework install command."""
-        version_to_get = options["use_version"] if "use_version" in options else config.get_version()
+        version_to_get = options["use_version"] if "use_version" in options and options["use_version"] is not None else config.get_version()
 
         if "static_folder" not in options:
             print("Error: no static folder provided. Use --static-folder or add a STATIC_ROOT to settings.")
@@ -41,8 +41,54 @@ class Command(BaseCommand):
             urllib.request.urlretrieve(config.get_min_css_url(version_to_get), os.path.join(options["static_folder"],
                                                                                             config.get_local_css_path(
                                                                                                 version_to_get)))
+        else:
+            Path(options["static_folder"]).mkdir(parents=True, exist_ok=True)
+            npm_result = subprocess.run(["npm", "install", "--prefix", options['static_folder'], f"vanilla-framework@{version_to_get}"])
 
-        if "use_version" in options:
+            if npm_result.returncode != 0:
+                print("Error: npm failed to download vanilla-framework. Make sure you have npm installed and in your PATH (https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)")
+                return
+
+            vanilla_filename = os.path.join(options["static_folder"], config.get_local_sass_path())
+            if Path(vanilla_filename).is_file():
+                print(f"Note: {config.get_local_sass_path()} already exists, leaving it alone.")
+                print()
+            else:
+                vanilla_file = open(vanilla_filename, "w")
+                vanilla_file.write(
+                    """// Override default Vanilla settings here,
+                    
+                    
+                    // Import the framework
+                    @import 'node_modules/vanilla-framework';
+
+                    // Include Vanilla Framework
+                    @include vanilla;
+                    """.replace("                    ", ""))
+                vanilla_file.close()
+
+            if 'compressor' not in settings.INSTALLED_APPS:
+                print("The sass version of vanilla-framework uses django-compressor. Add 'compressor' to your")
+                print("INSTALLED_APPS list in your settings file, and make sure django-compressor is installed")
+                print()
+
+            scss_precomp_available = False
+            if hasattr(settings, "COMPRESS_PRECOMPILERS"):
+                for precompiler in settings.COMPRESS_PRECOMPILERS:
+                    if precompiler[0] == 'text/x-scss':
+                        scss_precomp_available = True
+                        break
+
+            if not scss_precomp_available:
+                print("The sass version of vanilla-framework needs a sass precompiler to work with Django. Install")
+                print("one, such as django-libsass, then add it to your COMPRESS_PRECOMPILERS list in settings.")
+                print("For example, using django-libsass, add:")
+                print("COMPRESS_PRECOMPILERS = (")
+                print("    ('text/x-scss', 'django_libsass.SassCompiler'),")
+                print(")")
+                print()
+
+        if "use_version" in options and options["use_version"] is not None:
             print("You have downloaded a specific version of Vanilla Framework. To use it, add:")
             print(f"VANILLAFRAMEWORK_VERSION = {version_to_get}")
             print("to your settings file.")
